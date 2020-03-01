52dcb40e5e4ed51848aeb36db947491ddf16b866
Eva Brinkmann <brinkmann.eva@web.de>
2020-03-01 17:35:31 +0100

    First setup, styled-components, prettierrc, font, userData

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export default function App() {
+  return (
+    <div>
+      <Title>hello capstone project</Title>
+    </div>
+  )
+}
+
+const Title = styled.h1`
+  font-family: 'Open Sans';
+  color: cornflowerblue;
+`
---
+++ src/userData.js
@@ -0,0 +1,24 @@
+const userData = [
+  {
+    id: 1,
+    status: 'alumni',
+    name: 'Natalie Fuchs',
+    title: 'Frontend Developer',
+    company: 'Argonauten GmbH',
+    country: 'Hamburg, Germany',
+    email: 'natalie.fuchs@argonauten.de',
+    capstoneProject: 'Stadt, Land, Fuchs',
+    personalWebsite: 'https://github.com/ntlfox/personal-website',
+    github: 'https://github.com/ntlfox',
+    codepen: 'https://codepen.io/ntlfox/',
+  },
+]
+
+const skills = [
+  { topic: 'html' },
+  { topic: 'css' },
+  { topic: 'sass' },
+  { topic: 'bootstrap' },
+  { topic: 'javascript' },
+  { topic: 'react' },
+]
