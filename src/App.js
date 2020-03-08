aca7159b8a262224a34dd108444795cad75480f0
evabrinkmann <brinkmann.eva@web.de>
2020-03-08 15:12:46 +0100

    Add header, main, footer with styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
 import React from 'react'
+import styled from 'styled-components/macro'
+
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Navigation from './Navigation'
 import HomePage from './HomePage'
 import userData from './userData'
+import CardList from './components/CardList'
 
 export default function App() {
   return (
-    <Router>
-      <div>
-        <Navigation />
-        <Switch>
-          <Route exact path="/">
-            <HomePage userData={userData} />
-          </Route>
-          <Route path="/create">
-            <section>Create</section>
-          </Route>
-        </Switch>
+    <AppGrid>
+      <StyledHeader>Profile Pool</StyledHeader>
+      <div style={{ overflowY: 'scroll', scrollBehavior: 'smooth' }}>
+        <StyledMain>
+          <CardList userData={userData} />
+        </StyledMain>
       </div>
-    </Router>
+      <StyledFooter>
+        <p>Profile Pool</p>
+        <p>Create Profile</p>
+      </StyledFooter>
+    </AppGrid>
   )
 }
+
+const StyledHeader = styled.header`
+  display: grid;
+  position: fixed;
+  width: 100%;
+  z-index: 1;
+  top: 0;
+  line-height: 2.2;
+  background: #549dd8;
+  box-shadow: 0 5px 20px 1px lightgrey;
+  font-weight: bold;
+  text-align: center;
+  font-size: 27px;
+  color: white;
+`
+const StyledMain = styled.main`
+  display: flex;
+  flex-direction: column;
+  margin: 60px 10px 30px;
+`
+const StyledFooter = styled.footer`
+  display: grid;
+  grid-template-columns: repeat(2, auto);
+  grid-column-end: 2;
+  gap: 1px;
+  position: fixed;
+  width: 100%;
+  z-index: 1;
+  bottom: 0;
+  line-height: 1.4;
+  background: #549dd8;
+  box-shadow: 0 -5px 20px 1px lightgrey;
+  color: white;
+
+  p {
+    flex-grow: 1;
+    color: white;
+    text-decoration: none;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-weight: bold;
+  }
+`
+const AppGrid = styled.div`
+  display: grid;
+  grid-template-rows: auto 48px;
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  height: 100%;
+`
--- src/Navigation.js
+++ src/Navigation.js
@@ -1,48 +1,48 @@
-import React, { useEffect, useState } from 'react'
-import { NavLink } from 'react-router-dom'
-import styled from 'styled-components/macro'
+// import React, { useEffect, useState } from 'react'
+// import { NavLink } from 'react-router-dom'
+// import styled from 'styled-components/macro'
 
-const Button = styled(NavLink)`
-  flex-grow: 1;
-  color: white;
-  text-decoration: none;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background: grey;
-  opacity: 0.9;
+// export default function Navigation() {
+//   const [toggle, setToggle] = useState(true)
 
-  &.active {
-    background: #549dd8;
-    opacity: 0.9;
-  }
-`
+//   useEffect(() => {
+//     console.log('Effect Navigation')
+//   }, [toggle])
 
-const NavigationStyled = styled.nav`
-  display: grid;
-  grid-template-columns: repeat(2, auto);
-  grid-column-end: 2;
-  position: fixed;
-  width: 100%;
-  z-index: 1;
-  top: 0;
-  line-height: 2.4;
-  box-shadow: 0 5px 7px 1px lightgrey;
-`
+//   return (
+//     <NavigationStyled onClick={() => setToggle(!toggle)}>
+//       <Button exact to="/">
+//         Home
+//       </Button>
+//       <Button to="/create">Create</Button>
+//     </NavigationStyled>
+//   )
+// }
 
-export default function Navigation() {
-  const [toggle, setToggle] = useState(true)
+// const Button = styled(NavLink)`
+//   flex-grow: 1;
+//   color: white;
+//   text-decoration: none;
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+//   background: grey;
+//   opacity: 0.9;
 
-  useEffect(() => {
-    console.log('Effect Navigation')
-  }, [toggle])
+//   &.active {
+//     background: #549dd8;
+//     opacity: 0.9;
+//   }
+// `
 
-  return (
-    <NavigationStyled onClick={() => setToggle(!toggle)}>
-      <Button exact to="/">
-        Home
-      </Button>
-      <Button to="/create">Create</Button>
-    </NavigationStyled>
-  )
-}
+// const NavigationStyled = styled.nav`
+//   display: grid;
+//   grid-template-columns: repeat(2, auto);
+//   grid-column-end: 2;
+//   position: fixed;
+//   width: 100%;
+//   z-index: 1;
+//   top: 0;
+//   line-height: 2.4;
+//   box-shadow: 0 5px 7px 1px lightgrey;
+// `
