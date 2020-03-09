fedba99e28c197861f94c039e98d86951c82cd2a
evabrinkmann <brinkmann.eva@web.de>
2020-03-09 11:34:04 +0100

    Add form

--- src/pages/CreatePage.js
+++ src/pages/CreatePage.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import styled from 'styled-components/macro'
 import Header from '../components/Header'
 
 export default function CreatePage() {
   return (
     <>
       <Header headerTitle="Create Profile" />
+      <form style={{ marginTop: '100px' }}>
+        <label>
+          User Name
+          <input autoFocus name="question" placeholder="What's your name?" />
+        </label>
+        <label>
+          <div>Title</div>
+          <input name="answer" placeholder="What's your title?" />
+        </label>
+        <button>Create card</button>
+      </form>
     </>
   )
 }
