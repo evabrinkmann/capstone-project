78c9d6e69d393cea7750eb2bbbf74b7edbb25dfe
evabrinkmann <brinkmann.eva@web.de>
2020-03-09 13:37:53 +0100

    Edit storybook import global styles in each story

--- src/StorybookStyles.js
+++ src/StorybookStyles.js
@@ -1,17 +1,17 @@
-import React from 'react'
+// import React from 'react'
 
-const BodyStyle = {
-  margin: 0,
-  background: '#EEE',
-  fontFamily: 'sans-serif, Open Sans',
-  fontSize: '18px',
-  lineHeight: 1.4,
-  overflowY: 'scroll',
-  height: '100vh',
-  width: '100vw',
-}
+// const BodyStyle = {
+//   margin: 0,
+//   background: '#EEE',
+//   fontFamily: 'sans-serif, Open Sans',
+//   fontSize: '18px',
+//   lineHeight: 1.4,
+//   overflowY: 'scroll',
+//   height: '100vh',
+//   width: '100vw',
+// }
 
-const GlobalStyleStorybook = ({ children }) => (
-  <div style={BodyStyle}>{children}</div>
-)
-export default GlobalStyleStorybook
+// const GlobalStyleStorybook = ({ children }) => (
+//   <div style={BodyStyle}>{children}</div>
+// )
+// export default GlobalStyleStorybook
--- src/stories/CardHead.stories.js
+++ src/stories/CardHead.stories.js
@@ -1,18 +1,25 @@
 import React from 'react'
 import CardHead from '../components/CardHead'
 import userData from '../userData'
-import GlobalStyleStorybook from '../StorybookStyles'
+import GlobalStyles from '../GlobalStyles'
 
 export default {
   title: 'components/CardHead',
   component: CardHead,
   decorators: [
-    storyFn => <GlobalStyleStorybook>{storyFn()}</GlobalStyleStorybook>,
+    storyFn => (
+      <>
+        <GlobalStyles />
+        {storyFn()}
+      </>
+    ),
   ],
 }
 
 const { status, name, imgUrl, title } = userData[1]
 
 export const UserCardHead = () => (
-  <CardHead status={status} title={title} img={imgUrl} name={name} />
+  <div style={{ width: '300px', height: '500px' }}>
+    <CardHead status={status} title={title} img={imgUrl} name={name} />
+  </div>
 )
