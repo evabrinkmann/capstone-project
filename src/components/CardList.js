d49b93433c0c2fdea4c8dfe0b68c7c6e63c4ce1b
evabrinkmann <brinkmann.eva@web.de>
2020-03-17 09:39:05 +0100

    Toggle bookmark favorite page

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import styled from 'styled-components/macro'
 import Card from './Card'
 import PropTypes from 'prop-types'
--- src/pages/FavoritePage.js
+++ src/pages/FavoritePage.js
@@ -3,11 +3,14 @@ import Header from '../components/Header'
 import Bookmark from '../components/Bookmark'
 import CardBody from '../components/CardBody'
 import styled from 'styled-components/macro'
+import { useToggle } from 'react-hooks-lib'
 
 export default function FavoritePage({ profiles, onBookmarkClick }) {
   const filterProfiles = profiles.filter(
     profile => profile.isBookmarked === true
   )
+  const { on, toggle } = useToggle(false)
+
   return (
     <>
       <Header title="Favorite Profiles" />
@@ -15,7 +18,7 @@ export default function FavoritePage({ profiles, onBookmarkClick }) {
         <StyledMain>
           {filterProfiles.length !== 0 ? (
             filterProfiles.map(profile => (
-              <ProfileCardStyled>
+              <ProfileCardStyled onClick={toggle} key={profile.id}>
                 <Bookmark
                   onBookmarkClick={onBookmarkClick}
                   id={profile.id}
@@ -28,17 +31,19 @@ export default function FavoritePage({ profiles, onBookmarkClick }) {
                   <h1>{profile.name}</h1>
                   <h2>{profile.title}</h2>
                 </StyledHead>
-                <CardBody
-                  company={profile.company}
-                  location={profile.location}
-                  email={profile.email}
-                  capstoneName={profile.capstoneName}
-                  capstoneLink={profile.capstoneLink}
-                  personalWebsite={profile.personalWebsite}
-                  github={profile.github}
-                  codepen={profile.codepen}
-                  skills={profile.skills}
-                />
+                {on && (
+                  <CardBody
+                    company={profile.company}
+                    location={profile.location}
+                    email={profile.email}
+                    capstoneName={profile.capstoneName}
+                    capstoneLink={profile.capstoneLink}
+                    personalWebsite={profile.personalWebsite}
+                    github={profile.github}
+                    codepen={profile.codepen}
+                    skills={profile.skills}
+                  />
+                )}
               </ProfileCardStyled>
             ))
           ) : (
