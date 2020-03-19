779eaf3f5fed2c6b176ee1d261602786e918a77a
evabrinkmann <brinkmann.eva@web.de>
2020-03-19 17:31:54 +0100

    Fix StartPage

--- src/pages/StartPage.js
+++ src/pages/StartPage.js
@@ -4,25 +4,28 @@ import styled from 'styled-components/macro'
 
 export default function StartPage() {
   return (
-    <StyledContainer>
-      <h1>
-        Welcome <br /> to <br /> newAlumni!
-      </h1>
-      <img src={fishgroup} alt="fish" />
-    </StyledContainer>
+    <>
+      <StyledContainer>
+        <h1>Welcome to newAlumni!</h1>
+        <img src={fishgroup} alt="fish" />
+      </StyledContainer>
+    </>
   )
 }
 
 const StyledContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  text-align: center;
+  height: 100vh;
   background: #549dd8;
-  color: white;
-
   h1 {
-    margin: 50px;
+    text-align: center;
+    margin: 220px 100px;
+    color: white;
     font-weight: lighter;
+    position: relative;
+  }
+  img {
+    position: absolute;
+    bottom: 300px;
+    left: 175px;
   }
 `
